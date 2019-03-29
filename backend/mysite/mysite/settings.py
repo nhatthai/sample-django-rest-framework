230da71f5cd71cf1e1325dd82feb5165a4bef9a9
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2019-03-29 14:09:46 +0700

    update settings

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'accounts',
+    'feeds',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
